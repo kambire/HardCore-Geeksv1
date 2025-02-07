a25ee1d781f15d5ec81fbce5bf868ed4870e5b3e
Cesar Augusto Benitez <kambire2007@gmail.com>
2025-02-07 16:18:42 -0300

    Update HardcoreGeeksPlayerScript.h

---
+++ src/HardcoreGeeksPlayerScript.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "ScriptMgr.h"
+#include "Player.h"
+#include "Config.h"
+#include "Chat.h"
+#include "WorldSession.h"
+
+class HardcoreGeeksPlayerScript : public PlayerScript
+{
+public:
+    HardcoreGeeksPlayerScript() : PlayerScript("HardcoreGeeksPlayerScript") {}
+    
+    // Resto del código igual que antes...
+};
